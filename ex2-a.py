5d9b1913a2cfc7193718443ba189bf599291ff18
maxouupicsou <144345958+maxzouupicsou@users.noreply.github.com>
2023-11-06 11:21:11 +0100

    Calcul des dépenses marketing mensuelles + Vérification du fonctionnement avec valeurs négatives et décimales

--- ex2-a.py
+++ ex2-a.py
@@ -1,12 +1,8 @@
-# Créez une liste des dépenses marketing mensuelles
-depenses_mensuelles = [1000, 1200, 800, 1500, 1100, 1300, 1400, 900, 1000, 950, 1200, 1100]
+depenses_mensuelles = [1000.20, 1200, 800, 1500, 1100, -12, 1400, 900, 1000, 950, 1200, -78000]
 
-# Initialisez une variable pour stocker les dépenses totales de l'année
 depenses_annuelles = 0
 
-# Parcourez la liste des dépenses mensuelles et additionnez-les
 for depense in depenses_mensuelles:
     depenses_annuelles += depense
 
-# Affichez les dépenses totales de l'année
 print(f"Les dépenses totales de l'année sont de : {depenses_annuelles} dollars")
