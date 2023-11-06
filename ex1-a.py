dad270170b994966823827b0592c9f5560d50e77
maxouupicsou <144345958+maxzouupicsou@users.noreply.github.com>
2023-11-06 08:48:15 +0100

    stockages des info de l'entreprise dans des variables

---
+++ ex1-a.py
@@ -0,0 +1,5 @@
+nom_entreprise = "Tartanpion"
+nombre_clients = 100
+revenu_moyen_par_client = 20
+revenu_total = revenu_moyen_par_client * nombre_clients
+print (f"le montant du revenu total est de {revenu_total}")
