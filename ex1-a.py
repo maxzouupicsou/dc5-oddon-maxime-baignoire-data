89b1a9210f17b1b3e92dcec7ca4db940b80ccf89
maxouupicsou <144345958+maxzouupicsou@users.noreply.github.com>
2023-11-12 22:41:42 +0100

    MAJ Gestion des erreurs

--- ex1-a.py
+++ ex1-a.py
@@ -1,5 +1,12 @@
-nom_entreprise = "Tartanpion"
-nombre_clients = 100
-revenu_moyen_par_client = 20
-revenu_total = revenu_moyen_par_client * nombre_clients
-print (f"le montant du revenu total est de {revenu_total}")
+try:
+    nombre_clients = 100
+    revenu_moyen_par_client = 20
+  
+    if not (isinstance(nombre_clients, (int, float)) and isinstance(revenu_moyen_par_client, (int, float))):
+        raise TypeError("Attention ! Le nombre de clients et le revenu moyen par client doit être entré au format numérique ")
+
+    revenu_total = revenu_moyen_par_client * nombre_clients
+    print(f"Le montant du revenu total est de {revenu_total}")
+
+except TypeError as e:
+    print(f"Erreur: {e}")
